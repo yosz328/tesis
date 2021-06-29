079ae2462edbc05f1196d6a18785f01c453bc588
yosz328 <44147404+yosz328@users.noreply.github.com>
2021-06-28 23:42:39 -0400

    Update PreguntasService.js

--- src/services/PreguntasService.js
+++ src/services/PreguntasService.js
@@ -1,6 +1,5 @@
 import Axios from 'axios';
 
-const url = 'http://localhost:9000';
 
 export const getPreguntas = () => {
     return Axios.get('https://mcafye3c94.execute-api.us-east-1.amazonaws.com/getTarjetas')
